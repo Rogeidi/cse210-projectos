9f7d82574f230c118fb600d14e954f2f9f97f960
Rogeidi <rogeidivirtual@gmail.com>
2026-09-12 21:54:18 -0300

    curriculo

--- semana02/Curriculo/Program.cs
+++ semana02/Curriculo/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Curriculo.");
+        Emprego emp1 = new Emprego();
+        emp1._cargo = "Engenheiro de Software";
+        emp1._empresa = "Microsoft";
+        emp1._anoInicio = 2019;
+        emp1._anoFim = 2022;
+
+        Emprego emp2 = new Emprego();
+        emp2._cargo = "Gerente de projetos";
+        emp2._empresa = "Google";
+        emp2._anoInicio = 2022;
+        emp2._anoFim = 2025;
+
+        Curriculo meuCurriculo = new Curriculo();
+        meuCurriculo._nome = "Carlos Silva";
+
+        meuCurriculo._empregos.Add(emp1);
+        meuCurriculo._empregos.Add(emp2);
+
+        meuCurriculo.ExibirCurriculo();
+
+
     }
 }
